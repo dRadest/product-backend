0fe2265cfd26a69b6608241402f202e76210f609
drad <progdrad@gmail.com>
2024-07-04 16:25:28 +0200

    Initial state of the project

---
+++ src/test/java/com/dradest/backend/product/ProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dradest.backend.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
